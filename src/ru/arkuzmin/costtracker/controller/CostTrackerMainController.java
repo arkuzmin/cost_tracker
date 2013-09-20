8e7d633e111014ff6986eb498b730eaa2a4fee14
ArKuzmin <kuzminarty@gmail.com>
2013-09-20 16:25:42 +0400

    Проверка работы с интерфейсом

--- src/ru/arkuzmin/costtracker/controller/CostTrackerMainController.java
+++ src/ru/arkuzmin/costtracker/controller/CostTrackerMainController.java
@@ -1,13 +1,20 @@
 package ru.arkuzmin.costtracker.controller;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import ru.arkuzmin.costtracker.db.bean.Agent;
 import ru.arkuzmin.costtracker.db.model.impl.AgentServiceImpl;
 
@@ -17,9 +24,21 @@ public class CostTrackerMainController implements Initializable {
 	Button newAgent;
 	@FXML
 	TextField agentName;
+	@FXML
+	TableView<Agent> table;
+	@FXML
+	TableColumn<Agent, String> colName;
+	@FXML
+	TableColumn<Agent, Button> colDel;
+	@FXML
+	TableColumn<Agent, Button> colUpd;
+	@FXML
+	ChoiceBox<Agent> choice;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		configureTable();
+		configureChoice();
 	}
 
 	public void addAgent(ActionEvent event) {
@@ -28,5 +47,39 @@ public class CostTrackerMainController implements Initializable {
 		agent.setName(agentName.getText());
 		service.addAgent(agent);
 		agentName.clear();
+		configureTable();
+		configureChoice();
 	}
+
+	private void configureChoice() {
+		AgentServiceImpl service = new AgentServiceImpl();
+		List<Agent> list = service.getAllAgents();
+		ObservableList<Agent> tableContent = FXCollections.observableList(list);
+		choice.setItems(tableContent);
+	}
+	
+	private void configureTable() {
+		colName.setCellValueFactory(new PropertyValueFactory<Agent, String>("name"));
+		
+		colName.setPrefWidth(40);
+		colDel.setPrefWidth(20);
+		colUpd.setPrefWidth(20);
+
+		colName.setMinWidth(40);
+		colDel.setMinWidth(20);
+		colUpd.setMinWidth(20);
+
+		colName.setMaxWidth(750);
+		colDel.setMaxWidth(500);
+		colUpd.setMaxWidth(500);
+
+		table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+		AgentServiceImpl service = new AgentServiceImpl();
+		List<Agent> list = service.getAllAgents();
+		ObservableList<Agent> tableContent = FXCollections.observableList(list);
+		
+		table.setItems(tableContent);
+	}
+
 }
